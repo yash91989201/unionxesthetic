c5f97343fc4ae5ead4282db98193b214151a6273
yash91989201 <yashraj6370@gmail.com>
2022-08-09 11:59:24 +0530

    fix : about & testimonial styling

--- components/home/AboutMe.tsx
+++ components/home/AboutMe.tsx
@@ -4,9 +4,9 @@ const AboutMe = () => {
   return (
     <section className="px-6 sm:px-12">
       <div className="max-w-6xl mx-auto py-16 sm:py-20 space-y-12 sm:space-y-20">
-        <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic">About Me</h2>
+        <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic xs:text-red-500">About Me</h2>
         <div className="flex flex-col lg:flex-row justify-evenly items-center space-y-8">
-          <div className="relative max-w-sm w-full md:w-1/2 h-[320px]  xs:h-[400px]  md:h-[480px] space-y-4">
+          <div className="relative">
             <Image
               src="/assets/images/aboutme_img.jpg"
               alt="about me img"

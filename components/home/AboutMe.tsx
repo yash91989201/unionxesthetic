9146f3bf89ca67dd23dd4d42d0823a38b36ca17c
yash91989201 <yashraj6370@gmail.com>
2022-08-09 12:28:49 +0530

    fix : update section styling

--- components/home/AboutMe.tsx
+++ components/home/AboutMe.tsx
@@ -4,9 +4,9 @@ const AboutMe = () => {
   return (
     <section className="px-6 sm:px-12">
       <div className="max-w-6xl mx-auto py-16 sm:py-20 space-y-12 sm:space-y-20">
-        <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic xs:text-red-500">About Me</h2>
+        <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic">About Me</h2>
         <div className="flex flex-col lg:flex-row justify-evenly items-center space-y-8">
-          <div className="relative">
+          <div className="relative max-w-sm w-80  sm:w-full md:w-2/5  h-[400px] sm:h-[480px] ">
             <Image
               src="/assets/images/aboutme_img.jpg"
               alt="about me img"
@@ -17,7 +17,7 @@ const AboutMe = () => {
             <h3 className="text-[clamp(24px,8vw,56px)] font-semibold italic text-amber-400">Hi, I am Amaan</h3>
             {/* paragraph text */}
             <p className=" text-[#666666] text-base font-medium leading-relaxed not-italic ">
-              A small river named Duden flows by their place and supplies it with the necessary regelialia.he coast of the Semantics, a large language ocean.
+              I am a certified fitness trainer , with my skills I have not only helped peoples to achieve a beautifully aesthetic body but also helped them to get into a fit lifestyle.
             </p>
           </div>
         </div>
--- components/home/WelcomeSection.tsx
+++ components/home/WelcomeSection.tsx
@@ -17,7 +17,7 @@ const WelcomeSection = () => {
                 <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic">WELCOME TO <span className="text-amber-400">UNION XESTHETICS</span></h2>
                 <div className="flex flex-col md:flex-row items-center space-y-8">
                     {/* video board */}
-                    <div className="w-full  md:w-1/2  flex flex-col  space-y-4">
+                    <div className="w-full md:w-1/2 flex flex-col space-y-4">
                         <iframe
                             className="w-full aspect-video"
                             src="https://youtube.com/embed/qZY0d1-Qzpc"
@@ -26,10 +26,10 @@ const WelcomeSection = () => {
                         </iframe>
                     </div>
                     <div className="p-0 md:p-12 w-full md:w-1/2  flex flex-col space-y-4 md:space-y-6">
-                        <h3 className="text-xl md:text-3xl  font-semibold italic text-amber-400">ONLINE FITNESS TRAINING</h3>
+                        <h3 className="text-[clamp(24px,6vw,40px)]  font-semibold italic text-amber-400">ONLINE FITNESS TRAINING</h3>
                         {/* paragraph text */}
-                        <p className=" text-[#666666] text-base font-medium leading-relaxed not-italic ">
-                            A small river named Duden flows by their place and supplies it with the necessary regelialia.he coast of the Semantics, a large language ocean.
+                        <p className=" text-gray-700 text-base font-medium leading-relaxed not-italic ">
+                            Union Xesthetic Fitness training program provides personalized fitness plan for muscle building and fat loss. This is most comprehensive program and trains you for complete health, fitness and performance. Your improvement will happen in all components of physical fitness. You will not only be able to do daily tasks efficiently, but also take part in competitive sports.
                         </p>
                         {/* join link */}
                         <Link href="https://wa.me/7205539494?text=Hi+%2C+I+am+interested+in+your+fitness+training+program+.+What+kind+of+fitness+plans+do+you+offer+%3F">

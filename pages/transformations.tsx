08bd2e7fb1148f6c50710a7c2e687901eb3ae1ed
yash91989201 <yashraj6370@gmail.com>
2022-08-09 19:25:31 +0530

    fix : update styling

--- pages/transformations.tsx
+++ pages/transformations.tsx
@@ -17,13 +17,13 @@ const ReviewSection = ({ allTransformation }: PageProps) => {
                 title="Client Transformations"
                 description=""
             />
-            <section className="px-6 sm:px-12">
+            <section className="px-3 xs:px-6 sm:px-12">
                 <div className="max-w-6xl mx-auto py-8 sm:py-16 space-y-10 sm:space-y-20 ">
                     <h2 className="text-[clamp(24px,8vw,56px)] font-bold text-center italic text-amber-400">Client Transformations</h2>
                     {/* testimony card */}
-                    <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-12 place-items-center sm:place-items-start">
+                    <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-6 place-items-center sm:place-items-start">
                         {
-                            allTransformation?.map(transformation => <div key={transformation._id} className="relative w-60 aspect-square">
+                            allTransformation?.map(transformation => <div key={transformation._id} className="relative w-56 xs:w-64 sm:w-full aspect-square">
                                 <Image
                                     src={urlFor(transformation.clientTransformationImage).url()!}
                                     alt=""

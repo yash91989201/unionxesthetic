009a2cf7e48fe4dd3d947f22c280534548399dee
yash91989201 <yashraj6370@gmail.com>
2022-08-13 13:02:39 +0530

    fix : add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 import Layout from '../components/shared/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return <>
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script id="google-analytics" strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+    </Script>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
 }
 
 export default MyApp
